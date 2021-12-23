c2738220054c036f1b02f00bcc264d59fe92e11a
向山　翔 <hms0175km@gmail.com>
2021-12-23 17:29:38 +0900

    [Add]リアクティブ

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-let vm1 = new Vue({
-    el:'#app1',
-    data:{
-        message: 'インスタンス1'
+// let vm1 = new Vue({
+//     el:'#app1',
+//     data:{
+//         message: 'インスタンス1'
+//     }
+// })
+
+// let vm2 = new Vue({
+//     el:'#app2',
+//     data:{
+//         message: 'インスタンス2'
+//     },
+//     methods: {
+//         changeMessage1: function () {
+//             vm1.message = 'インスタンス2から変えました'
+//       }
+//     },
+// })
+
+let vm = new Vue({
+    el: '#app',
+    data: {
+        message: 'こんにちは',
+        name: '名前です'
     }
 })
 
-let vm2 = new Vue({
-    el:'#app2',
-    data:{
-        message: 'インスタンス2'
-    },
-    methods: {
-        changeMessage1: function () {
-            vm1.message = 'インスタンス2から変えました'
-      }
-    },
-})
+// ゲッター = 変数が参照された時に関数を実行
+// セッター = メッセージが書き変わった時に関数を実行
