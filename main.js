37b2ac54d18339f865c14f5ca3c4115189ffda36
向山　翔 <hms0175km@gmail.com>
2021-12-23 17:01:27 +0900

    [Add]外側からインスタンスにアクセス

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-new Vue({
+let vm1 = new Vue({
     el:'#app1',
     data:{
         message: 'インスタンス1'
     }
 })
 
-new Vue({
+let vm2 = new Vue({
     el:'#app2',
     data:{
         message: 'インスタンス2'
-    }
+    },
+    methods: {
+        changeMessage1: function () {
+            vm1.message = 'インスタンス2から変えました'
+      }
+    },
 })
